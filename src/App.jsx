25ffbeeae88f41211390fec36edc4d0fd63eccdc
Miguel Urena <urena_miguel82@outlook.com>
2025-01-31 14:29:07 -0800

    Finish State and Hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
+import { useState } from 'react';
+
 const Card = ({ title }) => {
+  const [hasLiked, setHasLiked] = useState(false);
+
   return (
     <div className="card">
       <h2>{title}</h2>
+
+      <button onClick={() => setHasLiked(!hasLiked)}>
+        {hasLiked ? '❤️' : '🤍'}
+      </button>
     </div>
   )
 }
