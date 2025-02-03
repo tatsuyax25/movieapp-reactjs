54cf49bdccfab049d77d22c4aba29a9f77daa75e
Miguel Urena <urena_miguel82@outlook.com>
2025-02-03 14:35:16 -0800

    Added image

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,20 @@ import React from 'react'
 
 const App = () => {
   return (
-    <div>
-      <h1 class="text-3xl font-bold underline">Hello world!</h1>
-    </div>
+    <main>
+      <div className="pattern" />
+
+      <div className="wrapper">
+        <header>
+          <img src="/hero.png" alt="Hero Banner" />
+          <h1>
+            Find <span className='text-gradient'>Movies</span> You&apos;ll Enjoy Without the Hassle
+          </h1>
+        </header>
+
+        <p>Search</p>
+      </div>
+    </main>
   );
 }
 
