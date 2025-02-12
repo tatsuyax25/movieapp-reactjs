5339a94d1b19a7bddc106b33e2bd45135d4e9d37
Miguel Urena <urena_miguel82@outlook.com>
2025-02-12 14:53:33 -0800

    Connecting API

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,23 @@
-import Search from './components/Search';
-import { useState } from 'react';
+import { useEffect, useState } from "react";
+import Search from "./components/Search";
+
+const API_BASE_URL = 'https://api.themoviedb.org/3';
+
+const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
+
+const API_OPTIONS = {
+  method: 'GET',
+  headers: {
+    accept: 'application/json',
+  },
+}
 
 const App = () => {
-  const [searchTerm, setSearchTerm] = useState('');
+  const [searchTerm, setSearchTerm] = useState("");
+
+  useEffect(() => {
+
+  }, []);
 
   return (
     <main>
@@ -12,7 +27,8 @@ const App = () => {
         <header>
           <img src="/hero.png" alt="Hero Banner" />
           <h1>
-            Find <span className='text-gradient'>Movies</span> You&apos;ll Enjoy Without the Hassle
+            Find <span className="text-gradient">Movies</span> You&apos;ll Enjoy
+            Without the Hassle
           </h1>
         </header>
 
@@ -20,6 +36,6 @@ const App = () => {
       </div>
     </main>
   );
-}
+};
 
-export default App
+export default App;
