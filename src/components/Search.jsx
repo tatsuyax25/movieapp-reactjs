3ce5b96b6578ed08477aed6b882e659f478464a3
Miguel Urena <urena_miguel82@outlook.com>
2025-02-12 14:10:48 -0800

    Added search image and input

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
 
-const Search = () => {
+const Search = ({ searchTerm, setSearchTerm }) => {
   return (
-    <div className='text-white text-3xl'>Search</div>
+    <div className='search'>
+      <div>
+        <img src="search.svg" alt="search"/>
+
+        <input 
+          type='text'
+          placeholder='Search through our 1000+ movies...'
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+        />
+      </div>
+    </div>
   )
 }
 
