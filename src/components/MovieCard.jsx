916ff899d7a6c1caaaf1a4989a8dd9839534817c
Miguel Urena <urena_miguel82@outlook.com>
2025-02-15 13:45:34 -0800

    Staring movie card component

---
+++ src/components/MovieCard.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const MovieCard = ({ movie: { title, vote_average, poster_path, release_date, original_language } }) => {
+  return (
+    <div className='movie-card'>
+      <img src={poster_path ? `https://image.tmdb.org/t/p/w500/${poster_path}` : '/no-movie.png'} alt={title} />
+
+      <div className='mt-4'>
+        <h3>{title}</h3>
+      </div>
+    </div>
+  );
+}
+
+export default MovieCard
